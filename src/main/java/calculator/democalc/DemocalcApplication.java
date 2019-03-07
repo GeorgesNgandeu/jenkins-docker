374183a0178aa8595b8731c47d35ed0d85f04a7f
GeorgesNgandeu <georgesngandeu@gmail.com>
2019-03-07 14:38:12 +0100

    just commit for now

--- src/main/java/calculator/democalc/DemocalcApplication.java
+++ src/main/java/calculator/democalc/DemocalcApplication.java
@@ -2,20 +2,11 @@ package calculator.democalc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class DemocalcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemocalcApplication.class, args);
-	}
-
-}
-
-@Service
-public class Calculator {
-	int sum(int a, int b) {
-		return a + b;
 	}
 }
